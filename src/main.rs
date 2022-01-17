5187de475b0c9e0d1a49e4e938425b5d6b964007
JelNiSław <jelnislaw@gmail.com>
2022-01-17 22:50:08 +0100

    Add simplified regex matching

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use crossterm::style::{Color, Print, ResetColor, SetForegroundColor, Stylize};
+use crossterm::style::{Color, ResetColor, SetForegroundColor, Stylize};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, execute, QueueableCommand};
+use lazy_static::lazy_static;
 use rand::Rng;
+use regex::Regex;
 use std::fs;
-use std::io::{self, BufRead, BufReader, Write};
+use std::io::{self, BufRead, Write};
 
 const FILENAME: &str = "poem.txt";
 const TEXT_WIDTH: u8 = 64;
 
 fn main() {
-    let mut stdin = BufReader::new(io::stdin()).lines();
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
     let mut stdout = io::stdout();
 
     let file = fs::File::open(FILENAME)
         .unwrap_or_else(|err| panic!("failed reading \"{FILENAME}\": {err}"));
-    let file = BufReader::new(file);
+    let file = io::BufReader::new(file);
     let lines = file
         .lines()
         .enumerate()
@@ -26,8 +28,7 @@ fn main() {
         .enumerate()
         .fold(0, |correct_answers, (i, line)| {
             let (line, hidden_word) = hide_word(line);
-            stdout.queue(Print(line.blue())).unwrap();
-            stdout.queue(Print("> ")).unwrap();
+            print!("{}\n> ", line.blue());
             stdout.flush().unwrap();
             let input = stdin.next().unwrap().unwrap();
             let input = input.trim();
@@ -46,27 +47,29 @@ fn main() {
                 )
             };
             stdout.queue(SetForegroundColor(foreground_color)).unwrap();
-            stdout.queue(Print(format!("{message} "))).unwrap();
-            stdout
-                .queue(Print(format!("{correct_answers}/{}\n", i + 1)))
-                .unwrap();
+            println!("{message} {correct_answers}/{}", i + 1);
             stdout.queue(ResetColor).unwrap();
-            stdout
-                .queue(Print("-".repeat(TEXT_WIDTH as usize)))
-                .unwrap();
-            stdout.queue(Print("\n\n")).unwrap();
-            stdout.flush().unwrap();
+            println!("{}", "-".repeat(TEXT_WIDTH as usize));
             correct_answers
         });
 }
 
-// here we're using AsRef<str> instead of &str or String, because AsRef<str> can accept both of these.
 fn hide_word(line: impl AsRef<str>) -> (String, String) {
-    let line = line.as_ref();
-    let mut words = line.split_whitespace().collect::<Vec<_>>();
+    lazy_static! {
+        static ref WORD_RE: Regex = Regex::new(r"\w+").unwrap();
+    }
+
+    let mut words = line.as_ref().split_whitespace().collect::<Vec<_>>();
     let n = rand::thread_rng().gen_range(0..words.len());
-    let hidden_word = words[n].to_string();
-    words[n] = "___";
+
+    let word = words[n].to_string();
+    let capture = WORD_RE.find(&word).unwrap();
+    let hidden_word = capture.as_str().to_string();
+
+    let mut censored_word = word.clone();
+    censored_word.replace_range(capture.range(), "___");
+
+    words[n] = censored_word.as_str();
     let new_line = words.join(" ");
     (new_line, hidden_word)
 }
