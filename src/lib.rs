7399688b80fbf876b97fe30773beadca82881ba1
michalwa <michalwa2003@gmail.com>
2022-01-20 15:54:49 +0100

    Hide multiple words + refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use deunicode::deunicode;
+use rand::prelude::*;
 
 pub trait EqUnicodeInsensitive<T: ?Sized> {
     fn eq_unicode_insensitive(&self, rhs: &T) -> bool;
@@ -16,7 +17,69 @@ where
     /// assert!("Zęby".eq_unicode_insensitive("żeby"));
     /// ```
     fn eq_unicode_insensitive(&self, rhs: &T) -> bool {
-        deunicode(self.to_lowercase().as_ref())
-            == deunicode(rhs.as_ref().to_lowercase().as_ref())
+        deunicode(self.to_lowercase().as_ref()) == deunicode(rhs.as_ref().to_lowercase().as_ref())
+    }
+}
+
+pub trait SplitHuman<'a> {
+    type Item: 'a;
+    fn split_human(&'a self) -> Box<dyn Iterator<Item = Self::Item> + 'a>;
+}
+
+impl<'a> SplitHuman<'a> for str {
+    type Item = &'a str;
+
+    /// Splits a string on whitespace and trims non-alphabetic chars off of words
+    ///
+    /// ```
+    /// use poematic::SplitHuman;
+    /// let words = "Lorem ipsum? It's dolor, sit amet!"
+    ///     .split_human()
+    ///     .collect::<Vec<_>>();
+    /// assert_eq!(&words, &["Lorem", "ipsum", "It's", "dolor", "sit", "amet"]);
+    /// ```
+    fn split_human(&'a self) -> Box<dyn Iterator<Item = Self::Item> + 'a> {
+        Box::new(
+            self.split_whitespace()
+                .map(|s| s.trim_matches(|ch: char| !ch.is_alphabetic())),
+        )
+    }
+}
+
+/// Replaces `n` random words in the given string with the given blank string.
+/// Returns the resulting string and the selected words
+pub fn hide_words<'a>(sentence: &'a str, blank: &str, n: usize) -> (String, Vec<&'a str>) {
+    let mut rng = rand::thread_rng();
+    let mut result = sentence.to_string();
+    let mut hidden_words = vec![];
+
+    let mut words_to_hide = sentence
+        .split_human()
+        .enumerate()
+        .choose_multiple(&mut rng, n);
+
+    words_to_hide.sort_by_key(|(i, _)| *i);
+
+    for (_, word) in words_to_hide.into_iter().rev() {
+        // Safe because `word` always points to a subslice of `sentence`
+        let byte_offset = unsafe { word.as_ptr().offset_from(sentence.as_ptr()) as usize };
+        result.replace_range(byte_offset..(byte_offset + word.len()), blank);
+
+        hidden_words.push(word);
+    }
+
+    hidden_words.reverse();
+    (result, hidden_words)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hide_words() {
+        let (sentence, words) = hide_words("Lorem ipsum? Dolor sit amet!", "___", 3);
+        assert_eq!(words.len(), 3);
+        assert_eq!(sentence.matches("___").count(), 3);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use crossterm::style::{Color, Stylize};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, execute};
-use rand::Rng;
+use std::borrow::Cow;
 use std::fs;
 use std::io::{self, BufRead, Write};
+
 use poematic::*;
 
 const FILENAME: &str = "poem.txt";
+const DIFFICULTY: u8 = 3;
 const TEXT_WIDTH: u8 = 64;
 
 fn main() {
@@ -22,20 +24,27 @@ fn main() {
         .collect::<Vec<_>>();
 
     let mut correct_answers = 0;
+
     for (i, line) in lines.iter().enumerate() {
-        let (line, hidden_word) = hide_word(&line[..]);
+        let (line, hidden_words) = hide_words(&line, "___", DIFFICULTY as usize);
         print!("{}\n> ", line.with(Color::Blue));
         stdout.flush().unwrap();
+
         let input = stdin.next().unwrap().unwrap();
-        let input = input.trim();
-        let is_valid = input.eq_unicode_insensitive(&hidden_word);
+        let input_words = input.split_human().collect::<Vec<_>>();
+
+        let is_valid = hidden_words
+            .iter()
+            .zip(input_words.iter())
+            .all(|(&h, &i)| h.eq_unicode_insensitive(i));
+
         let (color, message) = if is_valid {
             correct_answers += 1;
-            (Color::Green, String::from("Correct answer!"))
+            (Color::Green, Cow::Borrowed("Correct answer!"))
         } else {
             (
                 Color::Red,
-                format!("Wrong answer! Correct: \"{hidden_word}\""),
+                format!("Wrong answer! Correct: {}", hidden_words.join(", ")).into(),
             )
         };
         println!(
@@ -44,22 +53,6 @@ fn main() {
         );
         println!("{}", "-".repeat(TEXT_WIDTH as usize));
     }
-}
-
-/// Selects a random word in the given string and replaces it with a blank: `___`.
-/// Returns the resulting string and the selected word
-fn hide_word(sentence: &str) -> (String, &str) {
-    let words = sentence.split_whitespace().collect::<Vec<_>>();
-
-    let idx = rand::thread_rng().gen_range(0..words.len());
-    let hidden_word = words[idx].trim_matches(|ch: char| !ch.is_alphabetic());
-
-    // Safe because `hidden_word` always points to a subslice of `sentence`
-    let byte_offset = unsafe { hidden_word.as_ptr().offset_from(sentence.as_ptr()) as usize };
-    let mut sentence = sentence.to_string();
-    sentence.replace_range(byte_offset..(byte_offset + hidden_word.len()), "___");
-
-    (sentence, hidden_word)
 }
 
 #[allow(dead_code)]
