88582ec1953e60a63e412c346f6a3f69f92c212c
jel <25802745+jelni@users.noreply.github.com>
2022-01-23 16:26:10 +0100

    Gradually increase the difficulty

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::{fs, iter};
 use poematic::*;
 
 const FILENAME: &str = "poem.txt";
-const DIFFICULTY: u8 = 3;
 const TEXT_WIDTH: u8 = 64;
 
 fn main() {
@@ -20,27 +19,33 @@ fn main() {
         .map(|l| l.expect("Failed parsing file"))
         .collect::<Vec<_>>();
 
-    let mut correct_answers = 0;
+    let mut words_to_hide = 1;
+    loop {
+        let mut correct_answers = 0;
+        for (i, line) in lines.iter().enumerate() {
+            let (line, hidden_words) = hide_words(&line, "___", words_to_hide as usize);
+            print!("{}\n> ", line.with(Color::Blue));
+            stdout.flush().unwrap();
 
-    for (i, line) in lines.iter().enumerate() {
-        let (line, hidden_words) = hide_words(&line, "___", DIFFICULTY as usize);
-        print!("{}\n> ", line.with(Color::Blue));
-        stdout.flush().unwrap();
+            let input = stdin.next().unwrap().unwrap();
 
-        let input = stdin.next().unwrap().unwrap();
+            if is_valid_guess(&input, &hidden_words) {
+                correct_answers += 1;
+                print!("{}", "Correct answer!".with(Color::Green));
+            } else {
+                print!(
+                    "{}",
+                    format!("Wrong answer! Correct: {}", hidden_words.join(", ")).with(Color::Red),
+                );
+            };
 
-        if is_valid_guess(&input, &hidden_words) {
-            correct_answers += 1;
-            print!("{}", "Correct answer! ".with(Color::Green));
-        } else {
-            print!(
-                "{}",
-                format!("Wrong answer! Correct: {} ", hidden_words.join(", ")).with(Color::Red),
-            );
-        };
+            println!(" {correct_answers}/{}", i + 1);
+            println!("{}", "-".repeat(TEXT_WIDTH as usize));
+        }
 
-        println!("{correct_answers}/{}", i + 1);
-        println!("{}", "-".repeat(TEXT_WIDTH as usize));
+        if correct_answers >= lines.len() {
+            words_to_hide += 1;
+        }
     }
 }
 
